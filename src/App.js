c65145f959b4e795c48e22161bf21752104a172a
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-06-19 11:31:53 -0300

    Franco Pedrazzi

--- src/App.js
+++ src/App.js
@@ -4,19 +4,41 @@ import ShoppingForm from './ShoppingForm';
 
 function App() {
   const [items, setItems] = useState([]);
-
-  const addItem = (item) => {
-    setItems([...items, item]);
+  const [item, setItem] = useState("");
+  const [aux, setaux] = useState(".");
+  const [categoría , setCategoría] = useState("Lacteo");
+  function addItem(newItem,categoría){
+    setItems([...items, newItem+" "+categoría]);
+    setaux("")
   };
-
+  const handleItem = (event) => {
+    setaux(event.target.value)
+    if(aux!=""){
+      setItem(event.target.value)
+    }
+    else{
+      event.target.value=""
+    }
+  };
+  const handleCategoría  = (event) => {
+    setCategoría(event.target.value)
+  };
   return (
     <div className="App">
       <h1>Lista de Compras</h1>
-      
-      <ShoppingForm addItem={addItem} />
+      <input id="input"type="text" onChange={handleItem}></input>
+      <select onChange={handleCategoría}>
+      <option value='Lacteo'>Lacteo</option>
+      <option value='Despensa'>Despensa</option>
+      <option value='Higiene'>Higiene</option>
+      <option value='Bebida'>Bebida</option>
+      <option value='Limpieza'>Limpieza</option>
+      <option value='Otro'>Otro</option>
+      </select>
+      <ShoppingForm addItem={()=>addItem(item,categoría)} />
 
       <ul>
-        {/* listar todos los objetos. Indicar tanto su nombre como su categoría. */}
+      {items.map((item) => <li>{item}</li>)}
       </ul>
     </div>
   );
